f67441caa37226e6a1a61c61b30fb86bdea53568
mastersjw <mastersjw@gmail.com>
2025-11-17 20:36:42 -0700

    Remove unused read-docx.js file

--- read-docx.js
+++
@@ -1,9 +0,0 @@
-const mammoth = require('mammoth');
-
-mammoth.extractRawText({path: 'Time Card (Admin).docx'})
-    .then(result => {
-        console.log(result.value);
-    })
-    .catch(err => {
-        console.error(err);
-    });
